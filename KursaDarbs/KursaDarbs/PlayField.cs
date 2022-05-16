25a3db5b7c84d0bc7de536551d5f7cf4bce1aae3
Krish F <krishidze@inbox.lv>
2022-05-16 19:00:21 +0300

    Ideas and draft

---
+++ KursaDarbs/KursaDarbs/PlayField.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursaDarbs
+// rinda = X*2=
+// kolona = Y*2+1=
+{
+    internal class PlayField
+    {
+        CellItems[,] grid = new CellItems[3,3];  
+       public const string GridLine = @"
+7│8│9
+─┼─┼─
+4│5│6
+─┼─┼─
+1│2│3
+";
+
+        public CellItems[,] Grid 
+        {
+            get => grid;
+            set
+            {
+                grid = value;
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
---
+++ KursaDarbs/KursaDarbs/Program.cs
@@ -0,0 +1,22 @@
+﻿// Tic Tac Toe by K and M
+
+
+// Grids no 9 laukumiem - vajag uzzimet  X
+// X│X│X   │ │ 
+// ─┼─┼─  ─┼─┼─
+// X│X│X   │ │ 
+// ─┼─┼─  ─┼─┼─
+// X│X│X   │ │ 
+// Grids ir piesaistits pie 2d masiva X
+// grid koordinates usable - 1,0 1,2 1,4
+// grid koordinates usable - 3,0 3,2 3,4
+// grid koordinates usable - 5,0 5,2 5,4
+// lai vieglak izmantos custom data dype = enum X
+// enum vajadzigs lai noteiktu X, O un NULL, atvieglos dzivi pie parbaudem
+// speles lauks bus klase
+// no speles lauka klases parbaudes tiks veiktas, vai ir ielikts, vai spele beigusies un ko darit talak
+
+
+using KursaDarbs;
+
+Console.WriteLine($"{PlayField.GridLine}");
